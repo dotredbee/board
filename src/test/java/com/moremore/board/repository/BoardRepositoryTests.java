028669d5271918d580a72f4276dc767de761b6ff
coderB <flack319@gmail.com>
2022-06-09 22:13:24 +0900

    find all board and reply with count about reply.

--- src/test/java/com/moremore/board/repository/BoardRepositoryTests.java
+++ src/test/java/com/moremore/board/repository/BoardRepositoryTests.java
@@ -5,6 +5,10 @@ import com.moremore.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
@@ -61,5 +65,18 @@ public class BoardRepositoryTests {
         for(Object[] arr : result) {
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> results = boardRepository.getBoardWithReplyCount(pageable);
+
+        results.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+
+            System.out.println(Arrays.toString(arr));
+        });
     }
 }
